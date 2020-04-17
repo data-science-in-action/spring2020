eec29215543b5ded688131c21254e571bcf46c66
Jun Yan <jun.yan@uconn.edu>
2020-04-17 18:19:59 +0800

    random seed set by the class

--- random.R
+++ random.R
@@ -1,5 +1,7 @@
 roster <- read.table("signup.md", sep = "|", skip = 2, header=FALSE,
                      col.names = c("date", "slot", "team", "members", "title"))
 teams <- sort(unique(trimws(roster$team)))
-set.seed(12345)
+set.seed(726156) ## seed jointly set by the class: first 6 digits
+## The order of presentations starting from
+## Friday, May 29, 2020
 sample(teams)
